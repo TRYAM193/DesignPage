f65711fea8756f693056e6543c051aa7bc3ecc73
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-08 22:04:43 +0530

    auto-sync: 2025-12-08 22:04:43

--- src/components/CanvasEditor.jsx
+++ src/components/CanvasEditor.jsx
@@ -421,7 +421,7 @@ export default function CanvasEditor({
         }
 
         existing.set({ ...objData.props });
-        active.setCoords();
+        existing.setCoords();
         fabricCanvas.requestRenderAll();
       } else {
 

ce41c6925e17856479dbecc3a7a8eea07889c458
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-08 21:57:07 +0530

    auto-sync: 2025-12-08 21:57:06

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -31,7 +31,7 @@ export default function EditorPanel() {
   const past = useSelector((state) => state.canvas.past);
   const future = useSelector((state) => state.canvas.future);
   const present = useSelector((state) => state.canvas.present);
-  console.log()
+  console.log(present);
   
   const [activePanel, setActivePanel] = useState('text'); 
   

f9421a35cced15edaefd722d79eb7a3dada01298
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-09 19:02:01 +0530

    auto-sync: 2025-12-09 19:02:01

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -221,7 +221,7 @@ export default function Toolbar({ id, type, object, updateObject, removeObject, 
             <button
               className={`style-button ${liveProps.fontWeight === 'bold' ? 'active' : ''}`}
               onClick={() => toggleTextStyle('bold')}
-              style={{backgroundColor: '#4949e5' ?}}
+              style={{backgroundColor: object.props.fontWeight === 'bold' ? '#4949e5' : ''}}
               title="Bold"
             >
               <FiBold size={16} />

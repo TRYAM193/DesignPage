6d9fa1b9ccc695101c8cdef7f28ceb4dc2b25bfd
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-10 16:40:05 +0530

    auto-sync: 2025-12-10 16:40:05

--- src/components/RightSidebarTabs.jsx
+++ src/components/RightSidebarTabs.jsx
@@ -45,17 +45,16 @@ export default function RightSidebarTabs(props) {
         )}
 
         {/* Placeholder for Layers tab */}
+        <h3 style={{ marginTop: '20px', textAlign: 'center', color: '#666' }}>Layers Panel</h3>
         {activeTab === 'layers' && (
-            <h3 style={{ marginTop: '20px', textAlign: 'center', color: '#666' }}>Layers Panel</h3>
-            {/* Layers Panel */}
+          {/* Layers Panel */ }
             {activeTab === 'layers' && (
-              <LayersPanel
-                selectedId={props.id}
-                setSelectedId={props.setSelectedId} // Pass setSelectedId function
-                fabricCanvas={props.fabricCanvas}
-              />
-            )}
-          </div>
+          <LayersPanel
+            selectedId={props.id}
+            setSelectedId={props.setSelectedId} // Pass setSelectedId function
+            fabricCanvas={props.fabricCanvas}
+          />
+        )}
         )}
       </div>
     </div>

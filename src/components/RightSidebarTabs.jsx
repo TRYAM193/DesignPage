c248582aae7b11dfd30d19ff65d75974f1610bc3
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-10 16:06:04 +0530

    auto-sync: 2025-12-10 16:06:04

--- src/components/RightSidebarTabs.jsx
+++ src/components/RightSidebarTabs.jsx
@@ -4,8 +4,8 @@ import Toolbar from './Toolbar'; // Imports your existing property editor compon
 import { FiLayers, FiSliders } from 'react-icons/fi'; // Icons for tabs
 import LayersPanel from './LayersPanel'
 
-export default function RightSidebarTabs({object, setSelectedId, fabricCanvas}) {
-  const props = object.props;
+export default function RightSidebarTabs(props) {
+  // State to manage the active tab: 'properties' or 'layers'
   const [activeTab, setActiveTab] = useState('properties');
 
   return (

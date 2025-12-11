8b350b19ea100ffa5d543db87df5385a053e0ef2
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-11 06:35:48 +0530

    auto-sync: 2025-12-11 06:35:48

--- src/objectAdders/Image.js
+++ src/objectAdders/Image.js
@@ -3,17 +3,19 @@ import { useState } from "react";
 export default async function Image(src, setSelectedId, setActiveTool, fabricCanvas) {
   const [addedImage, setAddedImage] = useState(false);
   if (!src) return;
-  
-  const id = Date.now()
-  
-  if(addedImage{const fabricImage = await FabricImage.fromURL(src, {
-    customId: id
-  });
 
-  setSelectedId(id)
-  setActiveTool('image')
-  setAddedImage(true);}
-  
+  const id = Date.now()
+
+  if (!addedImage) {
+    const fabricImage = await FabricImage.fromURL(src, {
+      customId: id
+    });
+
+    setSelectedId(id)
+    setActiveTool('image')
+    setAddedImage(true);
+  }
+
   if (fabricCanvas && addedImage) {
     fabricCanvas.add(fabricImage);
     fabricCanvas.setActiveObject(fabricImage);

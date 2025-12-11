5fedac6fa3ff72151c1f33e9d94833f2661ad4ce
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-11 06:22:56 +0530

    auto-sync: 2025-12-11 06:22:56

--- src/objectAdders/Image.js
+++ src/objectAdders/Image.js
@@ -3,7 +3,7 @@ import { FabricImage } from "fabric";
 export default async function Image({src, setSelectedId, setActiveTool}) {
   if (!src) return;
   
-
+  const id = Dtae
   
   const fabricImage = await FabricImage.fromURL(src, {
     customId: id

84b036402ceb6e11e69733a5c231342950c24475
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-11 07:06:35 +0530

    auto-sync: 2025-12-11 07:06:35

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -100,7 +100,7 @@ export default function EditorPanel() {
                                 className="top-bar-button"
                                 onClick={() => dispatch(undo())}
                                 disabled={past.length === 0}
-                                style={{}}
+                                style={{opacity: past.length === 0 ? 0.25 : 1}}
                             >
                                 <FiRotateCcw size={20} />
                             </button>

76d0759f7c22a5e5eeb151ae99dd8c027a80e4c9
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-11 06:49:35 +0530

    auto-sync: 2025-12-11 06:49:35

--- src/components/CanvasEditor.jsx
+++ src/components/CanvasEditor.jsx
@@ -467,6 +467,7 @@ export default function CanvasEditor({
         if (objData.type === 'text')
           newObj = StraightText(objData);
         if (objData.type === 'image')
+          ne
 
         if (newObj) {
           newObj.customId = objData.id;

ad4817b7f25fa88c5bafdd4dcf9c2d58e5d90988
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-11 06:56:54 +0530

    auto-sync: 2025-12-11 06:56:54

--- src/components/CanvasEditor.jsx
+++ src/components/CanvasEditor.jsx
@@ -467,7 +467,7 @@ export default function CanvasEditor({
         if (objData.type === 'text')
           newObj = StraightText(objData);
         if (objData.type === 'image')
-          if (existing.map(objData.id)
+          if (existing.map(obj objData.id)
           newObj = Image(objData.src, setSelectedId, setActiveTool, fabricCanvas);
         if (newObj) {
           newObj.customId = objData.id;

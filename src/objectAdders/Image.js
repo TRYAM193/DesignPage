7d3ad2d4afb201affc1f6fd024e91127e726bb8e
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-11 06:29:18 +0530

    auto-sync: 2025-12-11 06:29:17

--- src/objectAdders/Image.js
+++ src/objectAdders/Image.js
@@ -1,7 +1,7 @@
 import { FabricImage } from "fabric";
 import { useState } from "react";
 export default async function Image(src, setSelectedId, setActiveTool) {
-  const ad
+  const [addedImage, setAddedImage] = useState(false);
   if (!src) return;
   
   const id = Date.now()

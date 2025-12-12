da88538968ec64b18ff4704a167a5122d4e72956
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-12 21:48:30 +0530

    auto-sync: 2025-12-12 21:48:30

--- src/components/CanvasEditor.jsx
+++ src/components/CanvasEditor.jsx
@@ -431,7 +431,7 @@ export default function CanvasEditor({
       let existing = fabricObjects.find((o) => o.customId === id);
       // --- UPDATE EXISTING (Standard) ---
       if (existing) {
-        const existing.cus
+        if (existing)
 
         let updatesNeeded = {};
         for (const key in objData.props) {

4120c2b0884065888b183302081ef93b93cfeea3
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-12 21:57:53 +0530

    auto-sync: 2025-12-12 21:57:53

--- src/components/CanvasEditor.jsx
+++ src/components/CanvasEditor.jsx
@@ -433,7 +433,7 @@ export default function CanvasEditor({
       if (existing) {
         if (existing.customType === 'text') {
           fabricCanvas.remove(existing)
-          existing = null;
+          // existing = null;
         } else {
 
           let updatesNeeded = {};

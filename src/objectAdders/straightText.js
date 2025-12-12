45a0d563250603ffde4892cf10187fd138323f0b
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-12 21:54:56 +0530

    auto-sync: 2025-12-12 21:54:56

--- src/objectAdders/straightText.js
+++ src/objectAdders/straightText.js
@@ -5,7 +5,7 @@ export default function StraightText(obj) {
   const props = obj.props;
   return new FabricText(obj.props.text, {
     ...props,
-    customStyle: obj.textStyle,
+    customSType: obj.textStyle,
     customId: obj.id
   });
 }

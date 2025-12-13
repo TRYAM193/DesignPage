56200ac061d25aa846e0a5e91a015fb384cc4136
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-13 16:02:54 +0530

    auto-sync: 2025-12-13 16:02:54

--- src/components/CanvasEditor.jsx
+++ src/components/CanvasEditor.jsx
@@ -436,7 +436,7 @@ export default function CanvasEditor({
       let existing = fabricObjects.find((o) => o.customId === id);
       pastData = past.find(state => state.some(obj => obj.id === id)).find(obj => obj.id === objData.id);
       
-      if (pastData.props === objData.props) {
+      if ( pastData.props === objData.props) {
         return;
       }
 
